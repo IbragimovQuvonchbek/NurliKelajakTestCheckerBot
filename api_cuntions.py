8f3a4a12e418c26174e9a95399166e2730c57d02
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2024-08-21 22:26:29 +0500

    update

--- api_cuntions.py
+++ api_cuntions.py
@@ -2,7 +2,7 @@ import aiohttp
 
 
 async def check_test(name, solution, telegram_id, pk):
-    url = f"http://127.0.0.1:8000/api/v1/testchecker/check-test/{pk}/"
+    url = f"http://13.60.228.133/api/v1/testchecker/check-test/{pk}/"
     payload = {
         "solution": solution,
         "telegram_id": telegram_id,
@@ -14,21 +14,21 @@ async def check_test(name, solution, telegram_id, pk):
 
 
 async def get_all_tests():
-    url = "http://127.0.0.1:8000/api/v1/testchecker/all-tests/"
+    url = "http://13.60.228.133/api/v1/testchecker/all-tests/"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.json()
 
 
 async def get_test_by_id(pk):
-    url = f"http://127.0.0.1:8000/api/v1/testchecker/all-tests/{pk}/"
+    url = f"http://13.60.228.133/api/v1/testchecker/all-tests/{pk}/"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.json()
 
 
 async def user_solved(pk, telegram_id):
-    url = f"http://127.0.0.1:8000/api/v1/testchecker/user-solved/{pk}/?telegram_id={telegram_id}"
+    url = f"http://13.60.228.133/api/v1/testchecker/user-solved/{pk}/?telegram_id={telegram_id}"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.json()
