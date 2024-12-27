0bc4614780a85e73ec8b335a104cbeb9c13982e9
Quvonchbek <147556696+IbragimovQuvonchbek@users.noreply.github.com>
2024-12-27 14:59:02 +0500

    Update api_cuntions.py

--- api_cuntions.py
+++ api_cuntions.py
@@ -4,7 +4,7 @@ import aiohttp
 
 
 async def check_test(name, solution, telegram_id, pk):
-    url = f"http://13.60.228.133/api/v1/testchecker/check-test/{pk}/"
+    url = f"http://13.60.230.147/api/v1/testchecker/check-test/{pk}/"
     payload = {
         "solution": solution,
         "telegram_id": telegram_id,
@@ -16,28 +16,28 @@ async def check_test(name, solution, telegram_id, pk):
 
 
 async def get_all_tests():
-    url = "http://13.60.228.133/api/v1/testchecker/all-tests/"
+    url = "http://13.60.230.147/api/v1/testchecker/all-tests/"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.json()
 
 
 async def get_test_by_id(pk):
-    url = f"http://13.60.228.133/api/v1/testchecker/all-tests/{pk}/"
+    url = f"http://13.60.230.147/api/v1/testchecker/all-tests/{pk}/"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.json()
 
 
 async def user_solved(pk, telegram_id):
-    url = f"http://13.60.228.133/api/v1/testchecker/user-solved/{pk}/?telegram_id={telegram_id}"
+    url = f"http://13.60.230.147/api/v1/testchecker/user-solved/{pk}/?telegram_id={telegram_id}"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.json()
 
 
 async def add_file(pk, file_id):
-    url = f"http://13.60.228.133/api/v1/testchecker/add-testfile/"
+    url = f"http://13.60.230.147/api/v1/testchecker/add-testfile/"
     payload = {
         "test_id": pk,
         "file_id": file_id
